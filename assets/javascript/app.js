ccc843c511b2099106f0994b2e80adcc7863e0e3
Seth Keel <seth.keel@gmail.com>
2016-06-11 20:06:25 -0400

    third commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,47 @@
-// This .on("click") function will trigger the AJAX Call
-	$('#findAnimal').on('click', function(){
+$(document).ready(function() {
 
-		//Grabs the animal entered in the text box
-		var animal = $('#animal-input').val();
+	//Gif AJAX Query
 
-		//Query URL for Giphy API
-		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+	//On click function for animal buttons
+	$('.animalButton').on('click', function() {
 
-		// AJAX Query syntax
-			$.ajax({url: queryURL, method: 'GET'})
-	 
-	 		.done(function(response) {
+		//Define variables & Giphy API
+        var animal = $(this).data('animal');
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-	 			console.log(queryURL);
-	 			console.log(response);
+        //AJAX Query
+        $.ajax({
+                url: queryURL,
+                method: 'GET'
+            })
+            .done(function(response) {
 
-	 			// var results = response.data;
+            	//Console.log to see results
+                console.log(queryURL);
+                console.log(response)
 
-	 			// for (var i = 0; i < results.length; i++) {
-     			// console.log(results[i]) //prints the results of the for loop
+                // step 2: since the image information is inside of the data key then make a variable named results and set it equal to response.data
+
+                var results = response.data;
+
+                //Loop through results
+                for (var i = 0; i < results.length; i++) {
+
+                	 //Prepend results to page
+                    var animalDiv = $('<div>');
+                    var p = $('<p>').text("Rating: " + results[i].rating);
+                    var animalImage = $('<img>');
+                    animalImage.attr('src', results[i].images.fixed_height.url);
+                    animalDiv.append(p);
+                    animalDiv.append(animalImage);
+                    $('#gifArea').append(animalDiv);
+
+                }
+            });
+    });
 
 
-                // var animalDiv = $('<div>');
-                //     var p = $('<p>');
-                //     console.log(results[i].rating);
-                //     p.text(results[i].rating);
-
-                //     var animalImage = $('<img>')
-                //     animalImage.attr('src', results[i].images.fixed_height.url);
-
-                //     animalDiv.append(p);
-                //     animalDiv.append(animalImage);
-
-                //     $('#gifArea').prepend(animalDiv);
+});
 
 
-	 			// $('#movieView').html(myobject);
-	 			// $('#gifArea').html(JSON.stringify(response)); //convert objects into string
-
-
-			}); 
-		return false;
-	});
+//End of document.ready function
